0e23a809ca822d9d0769d52b72ae84ecf5208610
robP <robert.pinger@yahoo.com>
2025-10-18 14:29:51 -0700

    Minor reduction of redundant method calls.

--- conv_csv.py
+++ conv_csv.py
@@ -9,22 +9,25 @@ def convert_files() -> None:
     """ Serialize a csv into a Json. """
     pt: str = './files/'  # Begin in project directory for MVP
     _files: list = os.listdir(pt)
+    n: int = len(_files)
     obj_to_dump: list[dict] = []
+    failures: list = []
     count: int = 1 # track the current file for naming
 
     print(f'\n|{format_process_output('[Process Starting]', '一')}|')
-    progress_b(0, len(_files))
+    progress_b(0, n)
     for _file in _files:
-        progress_b(count, len(_files))
-
         # Iterate through each file in our list of files.
         file_path = os.path.join(pt, _file)
+
+        progress_b(count, n)
         with open(file_path, encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
             try:
                 obj_to_dump = [row for row in reader]
             except UnicodeDecodeError as e:
                 print(f'File {file_path} is still fucked:\n\t[{e}]')
+                failures.append(file_path)
 
         # Write object to the json file
         with open(f'./j_dir/_{_file.replace('.csv', '')}', 'w', newline='') as jsnfile:
@@ -32,9 +35,10 @@ def convert_files() -> None:
 
         count += 1
         obj_to_dump.clear()
+
     print(f'\n|{format_process_output('[Process Complete]', '一')}|')
-    _: list = os.listdir('./j_dir/')
-    fails: int = len(_files) - len(_)
-    summary(len(_files), fails)
+
+    fails: int = n - len(os.listdir('./j_dir/'))
+    summary(n, fails)
 
 convert_files()
